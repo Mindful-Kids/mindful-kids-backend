63f58a3a1460a11241e23398536afda5e5237260
Afzal <afzalashraf814@gmail.com>
2023-09-09 21:21:48 +0500

    💥 break: remove prisma

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require("express");
-const app = express();
-const { PrismaClient } = require("@prisma/client");
+require("dotenv").config();
+require("./config/db");
 
-const prisma = new PrismaClient();
+const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const allStudents = await prisma.student.findMany();
-  res.json(allStudents);
-});
-
-app.post("/", async (req, res) => {
-  const newStudent = await prisma.student.create({ data: req.body });
-  res.json(newStudent);
+  res.status(200).json({ message: "API is working" });
 });
 
 app.listen(3000, () => console.log(`Server is running on PORT ${3000}`));
